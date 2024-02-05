a778bf3ca5e5937546f47dc7426cff0ff65ee2e7
boxyboxy <eugenengboxiang@gmail.com>
2024-02-05 14:44:18 +0800

    first commit

---
+++ src/main/java/com/eugene/redditcrawler/controller/JsoupController.java
@@ -0,0 +1,21 @@
+package com.eugene.redditcrawler.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@RestController
+@RequestMapping(path="/", produces = MediaType.APPLICATION_JSON_VALUE)
+public class JsoupController {
+
+    @GetMapping("/test")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> test(){
+        return ResponseEntity.ok().build();
+    }
+
+}
---
+++ src/test/java/com/eugene/redditcrawler/RedditCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eugene.redditcrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditCrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
